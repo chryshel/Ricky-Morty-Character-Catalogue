74f4fbbe3c9db1cd4a08ef73e4294fce22eef191
irusri-dev <developer@irusri.com>
2025-05-17 10:30:35 +0530

    Initial Commit

---
+++ RandomCharacterApp/RandomCharacterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomCharacterAppApp.swift
+//  RandomCharacterApp
+//
+//  Created by chryshel on 2025-05-17.
+//
+
+import SwiftUI
+
+@main
+struct RandomCharacterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
